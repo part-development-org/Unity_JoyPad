a23e4fd80747e08b97ec7714eaa6033d59b11acf
TheProxor <proxortv@gmail.com>
2019-03-24 18:36:06 +0700

    Worked joypad

---
+++ JoyPad.cs
@@ -0,0 +1,83 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.EventSystems;
+
+[System.Serializable]
+public class JoyPad : MonoBehaviour, IDragHandler, IPointerUpHandler, IPointerDownHandler
+{
+
+    public static JoyPad instance;
+
+    public Image JoistickBackground, JoystickCircle;
+
+    public Vector3 _inputVector { get; private set; }
+
+
+    private bool dragged;
+
+    private void Awake()
+    {
+        instance = this;
+    }
+
+    public void OnPointerDown(PointerEventData e)
+    {
+        OnDrag(e);
+        dragged = true;
+    }
+
+    public void OnDrag(PointerEventData e)
+    {
+        Vector2 pos;
+        if (RectTransformUtility.ScreenPointToLocalPointInRectangle(JoistickBackground.rectTransform, e.position, e.pressEventCamera, out pos))
+        {
+
+            pos.x = (pos.x / JoistickBackground.rectTransform.sizeDelta.x);
+            pos.y = (pos.y / JoistickBackground.rectTransform.sizeDelta.y);
+
+            _inputVector = new Vector3(pos.x * 2.5f, 0, pos.y * 2.5f);
+            _inputVector = (_inputVector.magnitude > 1.0f) ? _inputVector.normalized : _inputVector;
+
+            JoystickCircle.rectTransform.anchoredPosition = new Vector3(_inputVector.x * (JoistickBackground.rectTransform.sizeDelta.x / 3),
+                                                                     _inputVector.z * (JoistickBackground.rectTransform.sizeDelta.y / 3));
+        }
+    }
+
+    public void OnPointerUp(PointerEventData e)
+    {
+        dragged = false;
+    }
+
+
+    public float Horizontal()
+    {
+        if (_inputVector.x != 0)
+        {
+            return _inputVector.x;
+        }
+
+        return Input.GetAxis("Horizontal");
+    }
+
+    public float Vertical()
+    {
+        if (_inputVector.z != 0)
+        {
+            return _inputVector.z;
+        }
+
+        return Input.GetAxis("Vertical");
+    }
+
+    private void Update()
+    {
+        if (!dragged)
+        {
+            JoystickCircle.rectTransform.anchoredPosition = Vector3.Lerp(JoystickCircle.rectTransform.anchoredPosition, Vector3.zero, 10 * Time.deltaTime);
+            _inputVector = Vector3.Lerp(_inputVector, Vector3.zero, 6.5f * Time.deltaTime);
+        }
+    }
+
+}
